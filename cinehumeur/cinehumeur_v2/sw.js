4242a65281367b616e24dd55d464f40ed87211f4
poulaoe <46631525+poulaoe@users.noreply.github.com>
2026-07-25 22:14:50 +0200

    Delete cinehumeur directory

--- cinehumeur/cinehumeur_v2/sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE="cinehumeur-clean-v1";
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"])))});
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
